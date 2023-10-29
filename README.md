3ebb393046a2b519c46a6a2c7d9a2de496d8c342
Sberm <1007273067@qq.com>
2023-10-29 23:34:09 +0800

    Change readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # transgender
+
+Simple terminal based file explorer, written by a ranger hater.
+
+## usage
+
+```bash
+trans
+```
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
 // read input
 // switch file in current dir -> re-render
 // switch file between parent and child dir -> push(pop) stack， switch to the first file in the child dir, re-render.
-
