4905e95cd826cb5b948312df7d8fcf8b060a56e3
Sberm <1007273067@qq.com>
2024-03-11 20:03:00 +0800

    Prototype, but with highlighting and basic browsing

--- src/browser.rs
+++ src/browser.rs
@@ -1,17 +1,19 @@
 use std::vec::Vec;
 use std::io::stdin;
 use std::fs::{read_dir};
+use std::path::Path;
 use crate::ops::code;
 use crate::canvas;
 
-struct Browser {
+struct Browser<'canvas> {
     cursor: usize,
     current_dir: Vec<String>, // for display
     past_dir: Vec<String>, // for popping back
+    canvas: &'canvas mut canvas::Canvas,
 }
 
-impl Browser {
-    fn start_loop(&mut self, canvas: &mut canvas::Canvas) {
+impl Browser<'_> {
+    fn start_loop(&mut self) {
         self.read_to_current_dir(&String::from("."));
         loop {
             match process_input() {
@@ -21,21 +23,60 @@ impl Browser {
                 code::RIGHT => {self.right();}
                 _ => {self.right();}
             }
-            canvas.draw();
+
+            /* for preview */
+            let preview_dir = self.get_preview();
+
+            /*
+            println!("\ncurrent:");
+            for i in 0..self.current_dir.len() {
+                println!("{}", self.current_dir[i]);
+            }
+
+            println!("\npreview:");
+            for i in 0..preview_dir.len() {
+                println!("{}", preview_dir[i]);
+            }
+            */
+
+            self.canvas.draw(self.cursor, &self.current_dir, &preview_dir);
         }
     }
 
+    fn get_preview(&self) -> Vec<String>{
+        let mut ret: Vec<String> = Vec::new();
+
+        /* if it's a file */
+        if Path::new(&self.current_dir[self.cursor]).is_dir() == false {
+            return ret
+        }
+
+        for entry in read_dir(&self.current_dir[self.cursor]).unwrap() {
+            let entry = entry.unwrap();
+            ret.push(entry.path().to_str().unwrap().to_string());
+        }
+        ret
+    }
+
     fn up(&mut self) {
+        if self.current_dir.is_empty() == true {
+            self.cursor = 0;
+            return
+        }
         self.cursor = if self.cursor as isize - 1 >= 0 {self.cursor - 1} else {0};
     }
 
     fn down(&mut self) {
+        if self.current_dir.is_empty() == true {
+            self.cursor = 0;
+            return
+        }
         let l = self.current_dir.len();
-        self.cursor = if self.cursor + 1 <= l - 1 {self.cursor + 1} else {l - 1};
+        self.cursor = if self.cursor + 1 < l {self.cursor + 1} else {l - 1};
     }
 
     fn left(&mut self) {
-        if self.past_dir.len() <= 0 {// < might not be necessary 
+        if self.past_dir.is_empty() == true {// < might not be necessary 
             return
         }
         let last_dir = self.past_dir.pop();
@@ -44,17 +85,25 @@ impl Browser {
     }
 
     fn right(&mut self) {
-        let dir_under_cursor = self.current_dir[self.cursor].clone();
+        let dir_under_cursor = &self.current_dir[self.cursor].clone();
+
+        if Path::new(dir_under_cursor).is_dir() == false {
+            return
+        }
+
         self.past_dir.push(self.current_dir[self.cursor].clone());
         self.cursor = 0;
-        self.read_to_current_dir(&dir_under_cursor);
+        self.read_to_current_dir(dir_under_cursor);
     }
 
     fn read_to_current_dir(&mut self, path: &String) {
-        self.current_dir.shrink_to(0);
+
+        self.current_dir.clear();
+
+        // println!("len {}", self.current_dir.len());
+
         for entry in read_dir(path).unwrap() {
             let entry = entry.unwrap();
-            // println!("{:?}", entry.path());
             self.current_dir.push(entry.path().to_str().unwrap().to_string());
         }
     }
@@ -86,13 +135,15 @@ fn process_input() -> u32{
 
 
 pub fn init() {
+    let mut canvas = canvas::init();
+
     let mut browser = Browser {
         cursor: 0,
         current_dir: Vec::new(),
         past_dir: Vec::new(),
+        canvas: &mut canvas,
     };
 
-    let mut canvas = canvas::init();
 
-    browser.start_loop(&mut canvas);
+    browser.start_loop();
 }
--- src/canvas.rs
+++ src/canvas.rs
@@ -2,6 +2,7 @@ extern crate libc;
 
 use std::mem;
 use self::libc::{c_ushort, ioctl, STDOUT_FILENO, TIOCGWINSZ};
+use std::any::type_name;
 
 pub struct Canvas {
     height: usize,
@@ -15,56 +16,166 @@ fn CSI(s: &str) -> String{
     ret
 }
 
+pub struct Extra {
+    pos: usize,
+    extra_str: String,
+}
+
 impl Canvas {
 
     fn clear_whole(&self) {
-
         let mut str_to_draw = String::from("");
 
-        /* clear screen */
-        for i in 0..self.height {
-            str_to_draw.push_str(&(0..self.width).map(|_| " ").collect::<String>());
-        }
+        str_to_draw.push_str(&(0..(self.width * self.height) as isize).map(|_| " ").collect::<String>());
 
         str_to_draw.push_str(&CSI("[1H"));
+
         print!("{}", str_to_draw);
     }
 
-    pub fn draw(&self) {
+    fn clear_pixels(&mut self) {
+        for i in 0..self.height {
+            for j in 0..self.width {
+                self.pixels[i][j] = ' ';
+            }
+        }
+    }
+
+    pub fn draw(&mut self, cursor: usize, current_dir: &Vec<String>, preview_dir: &Vec<String>) {
+
+        /* write pixel */
+
+        let w_t: usize = self.height - 1;
+        let w_b: usize = 0;
+
+        let l_w_l: usize = 0;
+        let l_w_r: usize = (self.width as isize / 10 * 6 - 1) as usize;
+
+        let r_w_l: usize = l_w_r + 1;
+        let r_w_r: usize = self.width - 1;
+
+        /* left side */
+        let mut dir_i: usize = 0;
+        let mut ch_i: usize = 0;
+
+        self.clear_pixels();
 
         let mut str_to_draw = String::from("");
+        let mut back: bool = false;
+
+        let mut extra: Vec<Extra> = Vec::new();
+
+        for i in w_b..w_t {
+            let c_a = current_dir[dir_i].chars().collect::<Vec<char>>();
+            ch_i = 0;
+            for j in l_w_l..l_w_r {
+                if ch_i >= c_a.len() {
+                    break
+                }
+                self.set(w_t - i, j, c_a[ch_i]);
+                ch_i += 1;
+            }
+
+            ch_i = 0;
+            dir_i += 1;
+            if dir_i >= current_dir.len() {
+                break
+            }
+        }
+
+        let mut offset = 0;
+
+        let highlight = CSI("[0;30m");
+        let highlight_bg = CSI("[47m");
+        let normal = CSI("[0;37m");
+        let normal_bg = CSI("[40m");
+
+        extra.push(Extra{
+            pos: cursor * self.width,
+            extra_str: highlight.clone(),
+        });
+        offset += highlight.len();
+
+        extra.push(Extra{
+            pos: cursor * self.width + offset,
+            extra_str: highlight_bg.clone(),
+        });
+        offset += highlight_bg.len();
+
+        extra.push(Extra{
+            pos: cursor * self.width + r_w_l + offset,
+            extra_str: normal.clone(),
+        });
+        offset += normal.len();
+
+        extra.push(Extra{
+            pos: cursor * self.width + r_w_l + offset,
+            extra_str: normal_bg.clone(),
+        });
+
+        /* right side(preview) */
+
+        dir_i = 0;
+        ch_i = 0;
+
+        for i in w_b..w_t {
+            if dir_i >= preview_dir.len() {
+                break
+            }
+            let c_a = preview_dir[dir_i].chars().collect::<Vec<char>>();
+            ch_i = 0;
+            for j in r_w_l..r_w_r {
+                if ch_i >= c_a.len() {
+                    break
+                }
+                self.set(w_t - i, j, c_a[ch_i]);
+                ch_i += 1;
+            }
+            ch_i = 0;
+            dir_i += 1;
+        }
+
+        /* start drawing */
 
         for line in &self.pixels {
-            let tmp = line.to_owned(); // copy
-            let tmp_s = tmp.iter().collect::<String>(); // Vec<char> -> String
+            let tmp_s = line.iter().collect::<String>(); // Vec<char> -> String
             str_to_draw.push_str(&tmp_s); // concat
         }
 
+        for ex in &extra {
+            str_to_draw.insert_str(ex.pos, &ex.extra_str);
+        }
+
         str_to_draw.push_str(&CSI("[1H"));
 
-        print!("\x1b[?25l");
         print!("{}", str_to_draw);
     }
 
-    pub fn set(&mut self, i: i32, j: i32, c: char) {
-        let i_to_write = self.height as i32 - i;
-        let j_to_write = j;
-        if 0 <= i_to_write && i_to_write < self.height as i32 &&
-            0 <= j_to_write && j_to_write < self.width as i32 {
-            self.pixels[i_to_write as usize][j_to_write as usize] = c;
+    fn set(&mut self, i: usize, j: usize, c: char) {
+        let i_to_write: i32 = self.height as i32 - 1 - i as i32;
+        let j_to_write: usize = j;
+        if 0 <= i_to_write && i_to_write < self.height as i32 && j_to_write < self.width {
+            self.pixels[i_to_write as usize][j_to_write] = c;
         }
     }
 
 }
 
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
 pub fn init() -> Canvas {
-    let (w, h) = term_size();
+    let (h, w) = term_size();
     let canvas = Canvas {
         height: h,
         width: w,
         pixels: vec![vec!['*'; w]; h],
     };
+    /* clear space for printing */
     canvas.clear_whole();
+    /* hide mouse */
+    print!("\x1b[?25l");
     canvas
 }
 
