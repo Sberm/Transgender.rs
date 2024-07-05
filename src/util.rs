c556cc755553151b123f66429ebf930f20fc3da0
Howard Chu <howardchu95@gmail.com>
2024-07-05 16:30:20 +0800

    Fix empty directory bug and cursor position bug

--- src/util.rs
+++ src/util.rs
@@ -2,6 +2,14 @@ extern crate libc;
 
 use self::libc::{termios, STDIN_FILENO, ECHO, ICANON, ISIG, tcgetattr, tcsetattr, TCSAFLUSH};
 use std::mem;
+use std::io::{self, Write};
+use std::time::Duration;
+use std::thread::sleep;
+
+#[allow(dead_code)]
+pub fn slp(i: u64) {
+    sleep(Duration::from_secs(i));
+}
 
 pub fn raw_input() {
     unsafe {
@@ -19,4 +27,18 @@ pub fn canonical_input() {
         termios_.c_lflag |= ECHO | ICANON | ISIG;
         tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_);
     }
+}
+
+pub fn enter_albuf() {
+    raw_input();
+    print!("\x1b[?25l"); // hide cursor
+    print!("\x1b[?1049h"); // use alternate buffer
+    let _ = io::stdout().flush();
+}
+
+pub fn exit_albuf() {
+    canonical_input();
+    print!("\x1b[?25h"); // show cursor
+    print!("\x1b[?1049l"); // switch back to normal screen buffer
+    let _ = io::stdout().flush();
 }
