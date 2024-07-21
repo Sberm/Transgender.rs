f357ad4443f1f3b02c2ce56beb2749f3aef5a473
Howard Chu <howardchu95@gmail.com>
2024-07-22 00:39:46 +0800

    When entering a directory, cursor will be at the top

--- src/browser.rs
+++ src/browser.rs
@@ -299,13 +299,8 @@ impl Browser {
         self.past_window_start.push(self.window_start);
         self.current_path = dir_under_cursor.clone();
         self.read_to_current_dir(&dir_under_cursor.to_str().unwrap().to_string());
-        let (h, _) = canvas::term_size();
-        self.cursor = (self.current_dir.len() - 1) / 2;
-        self.window_start = if self.cursor as isize - h as isize / 2 > 0 {
-            self.cursor - h / 2
-        } else {
-            0
-        };
+        self.cursor = 0;
+        self.window_start = 0;
     }
 
     fn read_to_current_dir(&mut self, path: &String) {
