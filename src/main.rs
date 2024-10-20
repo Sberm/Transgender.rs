e282d5396bc6be2dcfc8c09fd95c9ef55f13887e
Howard Chu <howardchu95@gmail.com>
2024-10-19 21:31:21 -0700

    Add -v option
    Former-commit-id: 881bc20b824a02cb5c1c758eb32ba42dd8a3152c

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,19 @@ mod canvas;
 mod ops;
 mod utf8;
 mod util;
+use std::env;
+use std::process::exit;
 
 fn main() {
     let mut canvas = canvas::new();
     let mut browser = browser::new();
+
+    /* -v */
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 && (args[1].eq("-v") || args[1].eq("--version")) {
+        println!("\n  Transgender.rs\n\n    Vanilla trans\n");
+        exit(0);
+    }
 
     util::enter_albuf();
     browser.start_loop(&mut canvas);
