1f9db154d1ae682576419e6b1b5deb33ebb74c78
Howard Chu <howardchu95@gmail.com>
2024-10-20 10:05:51 -0700

    Fix header styling issue
    Former-commit-id: ee65375a79687a630790c229945f58b5c6b6ba05

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 ║ Permission to use, copy, modify, and/or distribute this software for   ║
 ║ any purpose with or without fee is hereby granted, provided that the   ║
 ║ above copyright notice and this permission notice appear in all copies ║
-╚════════════════════════════════════════════════════════════════════════*/
+╚═══════════════════════════════════════════════════════════════════════*/
 
 mod browser;
 mod canvas;
