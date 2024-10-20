8f387f3a451befb840cd1933d92bbe544d4ab7cd
Howard Chu <howardchu95@gmail.com>
2024-10-20 08:57:35 -0700

    Add license header
    Former-commit-id: 3e65e0052b33344871ea09064c1098c3d05563cd

--- src/browser.rs
+++ src/browser.rs
@@ -1,3 +1,11 @@
+/*═══════════════════════════════════════════════════════════════════════╗
+║                          ©  Howard Chu                                 ║
+║                                                                        ║
+║ Permission to use, copy, modify, and/or distribute this software for   ║
+║ any purpose with or without fee is hereby granted, provided that the   ║
+║ above copyright notice and this permission notice appear in all copies ║
+╚════════════════════════════════════════════════════════════════════════*/
+
 use crate::canvas;
 use crate::ops::{code, consts, Mode, Ops};
 use crate::util;
