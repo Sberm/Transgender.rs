Sberm/Transgender.rs

commit c7898dd7dd9e496ec2998155cd9f1174f84f1752

Add bash completion for directories
Signed-off-by: Howard Chu <howardchu95@gmail.com>

src/main.rs
@@ -23,8 +23,19 @@ fn main() {
     // -v, --version
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
-        if args[1].eq("-v") || args[1].eq("--version") {
+        let option: &str = &args[1];
+        if option.eq("-v") || option.eq("--version") {
             println!("\n  Transgender.rs\n\n    Regex-powered trans\n");
+            exit(0);
+        } else if option.eq("--sh") {
+            let script = r#"
+ts () {
+  cd "$(transgender $1 3>&1 1>&2 2>&3 3>&- | tail -n 1)"
+}
+
+complete -o dirnames ts"#;
+
+            println!("{}", script);
             exit(0);
         } else {
             // otherwise the first argument will be identified as a path

commit 8f13b18b52ade0ed5a5ce3183ef4a44e9ed5b56f

fix: Fix shell eval bug (-bash: eval: line 18: syntax error near unexpected token `(')
When eval "$(transgender --sh)" is used, there is an error message:
-bash: eval: line 18: syntax error near unexpected token `('

Which is caused by not using "function func()" for definition of a shell
function, instead of using a func() for shell function. I wonder why this
is not allowed...

Fixes: c7898dd ("Add bash completion for directories")

Signed-off-by: Sberm <howardchu95@gmail.com>

src/main.rs
@@ -29,7 +29,7 @@ fn main() {
             exit(0);
         } else if option.eq("--sh") {
             let script = r###"
-ts () {
+function ts() {
   cd "$(transgender $1 3>&1 1>&2 2>&3 3>&- | tail -n 1)"
 }
 