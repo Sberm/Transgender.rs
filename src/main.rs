Sberm/Transgender.rs

commit 6e935c651bf337d154126db2c40c5c9da75db0fa

main: Use triple-hash for raw string, and add a new line
Mostly for better readability

Signed-off-by: Howard Chu <howardchu95@gmail.com>

src/main.rs
@@ -28,12 +28,13 @@ fn main() {
             println!("\n  Transgender.rs\n\n    Regex-powered trans\n");
             exit(0);
         } else if option.eq("--sh") {
-            let script = r#"
+            let script = r###"
 ts () {
   cd "$(transgender $1 3>&1 1>&2 2>&3 3>&- | tail -n 1)"
 }
 
-complete -o dirnames ts"#;
+complete -o dirnames ts
+            "###;
 
             println!("{}", script);
             exit(0);

commit 9b5ee56670d2cfadf0de50014ad7369fa1244bf2

chore: Fix zsh not using the same 'complete' command
Zsh does not use bash's 'complete' command for completion, instead it
has it's own zstyle thing. Fix this so zsh won't error out whenever a
new shell is started.

Fixes: 6e935c65

Signed-off-by: Howard Chu <howardchu95@gmail.com>

src/main.rs
@@ -33,10 +33,17 @@ function ts() {
   cd "$(transgender $1 3>&1 1>&2 2>&3 3>&- | tail -n 1)"
 }
 
-complete -o dirnames ts
-            "###;
+if [[ "$0" =~ .*bash$ ]];
+then
+  complete -o dirnames ts
+fi
 
-            println!("{}", script);
+if [[ "$0" == "zsh" ]];
+then
+  exit 0
+fi
+"###;
+            print!("{}", script);
             exit(0);
         } else {
             // otherwise the first argument will be identified as a path