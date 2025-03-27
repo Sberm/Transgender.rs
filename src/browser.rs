Sberm/Transgender.rs

commit 14afa9c5aa019f4c4f191414d080e2f8e5723433

chore: Do not read special characters such as Ctrl-C
Signed-off-by: Howard Chu <howardchu95@gmail.com>

src/browser.rs
@@ -333,17 +333,25 @@ impl Browser {
     fn search(&mut self, canvas: &mut canvas::Canvas) {
         let (mut chars, trunc, op) = util::read_chars_or_op(&self.trunc);
         self.trunc = trunc;
+        // regular text input
         if chars.len() != 0 {
-            // regular text input
-            let first_char = chars[0];
-            if first_char as usize == 27 {
+            let first_char = chars[0] as usize;
+            // for example, Ctrl + C = 3, Ctrl + I = 9 these characters cannot be displayed, yet
+            // they will take space in the search text
+            if first_char < 32 {
+                // escape or return (line feed)
+                if first_char != 27 && first_char != 10 {
+                    return;
+                }
+            }
+            if first_char == 27 {
                 // esc
                 self.mode = Mode::NORMAL;
                 self.search_history_index = self.search_history.len();
                 self.input_cursor_pos = 0;
                 canvas.reset_bottom_start();
                 return;
-            } else if first_char as usize == 127 {
+            } else if first_char == 127 {
                 // backspace
                 if self.input_cursor_pos >= 1 {
                     self.search_txt.remove(self.input_cursor_pos - 1);
@@ -356,7 +364,7 @@ impl Browser {
                     }
                     self.input_cursor_pos -= 1;
                 }
-            } else if first_char as usize == 10 {
+            } else if first_char == 10 {
                 // enter
                 self.save_history();
                 self.mode = Mode::NORMAL;

src/util.rs
@@ -283,8 +283,8 @@ pub fn read_chars_or_op(prev_trunc: &Vec<u8>) -> (Vec<char>, Vec<u8>, Op) {
     let mut _stdin = stdin();
     _stdin.read(&mut raw).expect("Failed to read");
     let (char_vec, trunc) = parse_utf8(&mut raw, prev_trunc);
-    if char_vec[0] as usize == 27 && char_vec.len() > 1 {
-        if char_vec[1] as usize == 91 && char_vec.len() > 2 {
+    if char_vec[0] as usize == 27 {
+        if char_vec.len() >= 3 && char_vec[1] as usize == 91 {
             match char_vec[2] as usize {
                 65 => return (vec![], trunc, Op::Up),
                 66 => return (vec![], trunc, Op::Down),

commit 8e9211a0cbc0938047109dff6bd1765346a48192

bug: Fix pressing Ctrl + Space in search mode causing panic
Fixes: 14afa9c5

Signed-off-by: Howard Chu <howardchu95@gmail.com>

src/browser.rs
@@ -344,7 +344,7 @@ impl Browser {
         let (_chars, trunc, op) = util::read_chars_or_op(&self.trunc);
         self.trunc = trunc;
         // regular text input
-        if op == Op::Noop {
+        if op == Op::Noop && _chars.is_some() {
             let mut chars = _chars.expect("unwrap char vec failed");
             let first_char = chars[0] as usize;
             // for example, Ctrl + C = 3, Ctrl + I = 9 these characters cannot be displayed, yet
@@ -423,7 +423,7 @@ impl Browser {
                 }
                 self.input_cursor_pos = self.search_txt.len();
             }
-        } else {
+        } else if op == Op::Left || op == Op::Right {
             // left and right arrow
             match op {
                 Op::Left => {

src/util.rs
@@ -326,6 +326,9 @@ pub fn read_chars_or_op(prev_trunc: &Vec<u8>) -> (Option<Vec<char>>, Vec<u8>, Op
     let mut _stdin = stdin();
     _stdin.read(&mut raw).expect("Failed to read");
     let (char_vec, trunc) = parse_utf8(&mut raw, prev_trunc);
+    if char_vec.is_empty() {
+        return (None, trunc, Op::Noop);
+    }
     if char_vec[0] as usize == 27 {
         if char_vec.len() >= 3 && char_vec[1] as usize == 91 {
             match char_vec[2] as usize {