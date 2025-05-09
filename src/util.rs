Sberm/Transgender.rs

commit 804a8a6e3a712d1d7aa60f9a8096127d1cdc4ef5

main: Add config file path argument for testing
Signed-off-by: Howard Chu <howardchu95@gmail.com>

src/util.rs
@@ -191,9 +191,14 @@ pub fn print_path(_path: &PathBuf, dest_file: Option<&PathBuf>) {
     }
 }
 
-pub fn get_theme() -> String {
+pub fn get_theme(_config_path: Option<&str>) -> String {
     if let Ok(home_dir) = var(consts::HOME_VAR) {
-        if let Ok(lines) = read_lines(&format!("{}/{}", home_dir, consts::CONFIG_FILE)) {
+        let config_path = if _config_path.is_some() {
+            _config_path.unwrap()
+        } else {
+            consts::CONFIG_FILE
+        };
+        if let Ok(lines) = read_lines(&format!("{}/{}", home_dir, config_path)) {
             for line in lines.flatten() {
                 let kv = line.split("=").collect::<Vec<&str>>();
                 if kv.len() != 2 {
@@ -212,16 +217,21 @@ pub fn get_theme() -> String {
 ///
 /// returns
 ///  opener's command with arguments
-pub fn get_opener(op: Op) -> (OsString, Option<Vec<OsString>>) {
+pub fn get_opener(op: Op, _config_path: Option<&str>) -> (OsString, Option<Vec<OsString>>) {
     let key = match op {
         Op::ExitCursorO => Some(consts::O_KEY),
         Op::ExitCursorEnter => Some(consts::ENTER_KEY),
         _ => None,
     };
+    let config_path = if _config_path.is_some() {
+        _config_path.unwrap()
+    } else {
+        consts::CONFIG_FILE
+    };
     let mut comm = OsString::from(consts::OPENER);
     let mut args: Option<Vec<OsString>> = None;
     if let Ok(home_dir) = var(consts::HOME_VAR) {
-        if let Ok(lines) = read_lines(&format!("{}/{}", home_dir, consts::CONFIG_FILE)) {
+        if let Ok(lines) = read_lines(&format!("{}/{}", home_dir, config_path)) {
             for line in lines.flatten() {
                 let kv = line.split("=").collect::<Vec<&str>>();
                 if kv.len() != 2 {

src/browser.rs
@@ -720,9 +720,9 @@ impl Opener {
     }
 }
 
-pub fn new(path: &str, dest_file: Option<String>) -> Browser {
-    let (comm_o, args_o) = util::get_opener(Op::ExitCursorO);
-    let (comm_enter, args_enter) = util::get_opener(Op::ExitCursorEnter);
+pub fn new(path: &str, dest_file: Option<String>, config_path: Option<&str>) -> Browser {
+    let (comm_o, args_o) = util::get_opener(Op::ExitCursorO, config_path);
+    let (comm_enter, args_enter) = util::get_opener(Op::ExitCursorEnter, config_path);
 
     let mut browser = Browser {
         cursor: 0,

src/canvas.rs
@@ -471,12 +471,12 @@ impl Canvas {
     }
 }
 
-pub fn new() -> Canvas {
+pub fn new(config_path: Option<&str>) -> Canvas {
     Canvas {
         height: 0,
         width: 0,
         pixels: Vec::new(),
-        theme: theme::Theme::from(&util::get_theme()),
+        theme: theme::Theme::from(&util::get_theme(config_path)),
         utf8_table: WcLookupTable::new(),
         bottom_start: 0,
         add_algnmt: false,

src/main.rs
@@ -22,6 +22,8 @@ const VERSION: &str = "1.4.2";
 fn main() {
     let mut path = String::from(".");
     let mut dest_file: Option<String> = None;
+    let mut config_path: String;
+    let mut config_path_op: Option<&str> = None;
 
     let mut args = env::args();
     let print_help_message = || {
@@ -31,6 +33,7 @@ fn main() {
     -v, --version  Print current version
     -h, --help     Show this message
     --sh           Print transgender configuration shell script
+    -c, --conf     Config file path (default: ~/.tsrc)
 
     Use transgender <DIR> to start transgender in DIR directory
 
@@ -87,6 +90,21 @@ fi
         } else if s.eq("-h") || s.eq("--help") {
             print_help_message();
             exit(0);
+        } else if s.eq("-c") || s.eq("--config") {
+            let _one_more = args.next();
+            let conf_err = || {
+                println!("-c/--config: Please provide the path to the configuration file");
+                print_help_message();
+                exit(0);
+            };
+            if _one_more.is_none() {
+                conf_err();
+            }
+            config_path = _one_more.expect("Failed to get config file path");
+            if !Path::new(&config_path).is_file() {
+                conf_err();
+            }
+            config_path_op = Some(&config_path);
         } else {
             // the starting path with no argument name
             let p = Path::new(&s);
@@ -97,8 +115,8 @@ fi
     }
 
     // multiple arguments with random order
-    let mut canvas = canvas::new();
-    let mut browser = browser::new(&path, dest_file);
+    let mut canvas = canvas::new(config_path_op);
+    let mut browser = browser::new(&path, dest_file, config_path_op);
 
     util::enter_albuf();
     browser.start_loop(&mut canvas);

commit fa4fc493e6f1ef63b46dcbfb07a127a5ae848bbd

args: Fix -c option
Before this fix, trans prepends the home directory to the config path,
this is not the correct logic. It should be either ~/.tsrc for default,
or the config path as is.

Fixes: 804a8a6e3a71 ("main: Add config file path argument for testing")
Signed-off-by: Howard Chu <howardchu95@gmail.com>

src/util.rs
@@ -192,21 +192,20 @@ pub fn print_path(_path: &PathBuf, dest_file: Option<&PathBuf>) {
 }
 
 pub fn get_theme(_config_path: Option<&str>) -> String {
-    if let Ok(home_dir) = var(consts::HOME_VAR) {
-        let config_path = if _config_path.is_some() {
-            _config_path.unwrap()
-        } else {
-            consts::CONFIG_FILE
-        };
-        if let Ok(lines) = read_lines(&format!("{}/{}", home_dir, config_path)) {
-            for line in lines.flatten() {
-                let kv = line.split("=").collect::<Vec<&str>>();
-                if kv.len() != 2 {
-                    continue;
-                }
-                if kv[0].trim().to_lowercase() == consts::THEME_KEY {
-                    return kv[1].trim().to_lowercase();
-                }
+    let config_path = if _config_path.is_some() {
+        _config_path.unwrap().to_string()
+    } else {
+        let home_dir = var(consts::HOME_VAR).expect("failed to get HOME env");
+        format!("{}/{}", home_dir, consts::CONFIG_FILE)
+    };
+    if let Ok(lines) = read_lines(&config_path) {
+        for line in lines.flatten() {
+            let kv = line.split("=").collect::<Vec<&str>>();
+            if kv.len() != 2 {
+                continue;
+            }
+            if kv[0].trim().to_lowercase() == consts::THEME_KEY {
+                return kv[1].trim().to_lowercase();
             }
         }
     }
@@ -224,43 +223,25 @@ pub fn get_opener(op: Op, _config_path: Option<&str>) -> (OsString, Option<Vec<O
         _ => None,
     };
     let config_path = if _config_path.is_some() {
-        _config_path.unwrap()
+        _config_path.unwrap().to_string()
     } else {
-        consts::CONFIG_FILE
+        let home_dir = var(consts::HOME_VAR).expect("failed to get HOME env");
+        format!("{}/{}", home_dir, consts::CONFIG_FILE)
     };
     let mut comm = OsString::from(consts::OPENER);
     let mut args: Option<Vec<OsString>> = None;
-    if let Ok(home_dir) = var(consts::HOME_VAR) {
-        if let Ok(lines) = read_lines(&format!("{}/{}", home_dir, config_path)) {
-            for line in lines.flatten() {
-                let kv = line.split("=").collect::<Vec<&str>>();
-                if kv.len() != 2 {
-                    continue;
-                }
-                if key.is_some() {
-                    if kv[0].trim().to_lowercase() == key.unwrap() {
-                        // "key =.*"
-                        let comm_op = kv[1].trim().split(" ").collect::<Vec<&str>>();
-                        if comm_op.len() != 0 {
-                            // key = code.*
-                            comm = OsString::from(comm_op[0]);
-                            args = Some(
-                                comm_op
-                                    .into_iter()
-                                    .skip(1)
-                                    .map(|x| OsString::from(x))
-                                    .collect(),
-                            );
-                            return (comm, args);
-                        }
-                    }
-                }
-                if kv[0].trim().to_lowercase() == consts::EDITOR_KEY
-                    || kv[0].trim().to_lowercase() == consts::OPENER_KEY
-                {
+    if let Ok(lines) = read_lines(&config_path) {
+        for line in lines.flatten() {
+            let kv = line.split("=").collect::<Vec<&str>>();
+            if kv.len() != 2 {
+                continue;
+            }
+            if key.is_some() {
+                if kv[0].trim().to_lowercase() == key.unwrap() {
+                    // "key =.*"
                     let comm_op = kv[1].trim().split(" ").collect::<Vec<&str>>();
                     if comm_op.len() != 0 {
-                        // can be overridden by 'o' or 'enter'
+                        // key = code.*
                         comm = OsString::from(comm_op[0]);
                         args = Some(
                             comm_op
@@ -269,7 +250,24 @@ pub fn get_opener(op: Op, _config_path: Option<&str>) -> (OsString, Option<Vec<O
                                 .map(|x| OsString::from(x))
                                 .collect(),
                         );
+                        return (comm, args);
                     }
+                }
+            }
+            if kv[0].trim().to_lowercase() == consts::EDITOR_KEY
+                || kv[0].trim().to_lowercase() == consts::OPENER_KEY
+            {
+                let comm_op = kv[1].trim().split(" ").collect::<Vec<&str>>();
+                if comm_op.len() != 0 {
+                    // can be overridden by 'o' or 'enter'
+                    comm = OsString::from(comm_op[0]);
+                    args = Some(
+                        comm_op
+                            .into_iter()
+                            .skip(1)
+                            .map(|x| OsString::from(x))
+                            .collect(),
+                    );
                 }
             }
         }