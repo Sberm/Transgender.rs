e4a193a91143822b7707d41dedb632f20f1c18cb
Howard Chu <howardchu95@gmail.com>
2025-06-10 09:04:04 -0700

    Build executables on Mac
    Signed-off-by: Howard Chu <howardchu95@gmail.com>

--- release.sh
+++ release.sh
@@ -1,29 +1,36 @@
+# This build script should be run on MacOS
+# to install necessary compilers, do:
+# brew install messense/macos-cross-toolchains/aarch64-unknown-linux-gnu
+
 TRANS=transgender
 V=
 
 function build () {
   cargo build --release --target=$1
-  if [ $1 = "x86_64-unknown-linux-gnu" ]; then
-	  V=$(target/$1/release/$TRANS -v | grep version | awk '{print $NF}')
+  # change below to your architecture
+  if [ $1 = "aarch64-apple-darwin" ]; then
+    V=$(target/$1/release/$TRANS -v | grep version | awk '{print $NF}')
   fi
   cp target/$1/release/$TRANS build/$TRANS-$V-$2
 }
 
 if [ ! -d build ]; then
-	mkdir build
+  mkdir build
 fi
 
-# x86_64 needs to be the first
+# rustup target add <target>
+# your local architecture needs to be the first
+build aarch64-apple-darwin          aarch64-apple-darwin
 build x86_64-unknown-linux-gnu      x86_64-linux-gnu
-build i686-unknown-linux-gnu        i686-linux-gnu
 build aarch64-unknown-linux-gnu     aarch64-linux-gnu
-build loongarch64-unknown-linux-gnu loongarch64-linux-gnu
-build riscv64gc-unknown-linux-gnu   riscv64gc-linux-gnu
+build x86_64-apple-darwin           x86_64-apple-darwin
 
 #### How to use the release binary
 #~~~sh
 #sudo cp transgender-<version>-<arch> /usr/local/bin/transgender
 #echo 'eval "$(transgender --sh)"' >> ~/.bashrc
+#source ~/.bashrc
 ## on zsh
 #echo 'eval "$(transgender --sh)"' >> ~/.zshrc
+#source ~/.zshrc
 #~~~
