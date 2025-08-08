9aadb1981d5e5dd8c60d39a905e9fff0b66cb471
Howard Chu <howardchu95@gmail.com>
2025-08-07 22:09:57 -0700

    main: Make help message, version, and shell script static
    Signed-off-by: Howard Chu <howardchu95@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,9 @@ use std::env;
 use std::path::Path;
 use std::process::exit;
 
-const VERSION: &str = "1.5.5";
+static VERSION: &str = "1.5.5";
 
-fn main() {
-    let mut path = String::from(".");
-    let mut dest_file: Option<String> = None;
-    let mut config_path: String;
-    let mut config_path_op: Option<&str> = None;
-
-    let mut args = env::args();
-    let print_help_message = || {
-        let help_message = r###"
+static HELP_MSG: &str = r###"
   transgender
     --dest <file>   File that transgender outputs the destination path to
     -v, --version   Print current version
@@ -38,25 +30,8 @@ fn main() {
     Use transgender <DIR> to start transgender in DIR directory
 
 "###;
-        print!("{}", help_message);
-    };
 
-    loop {
-        let _s = args.next();
-        if _s.is_none() {
-            break;
-        }
-
-        let s = _s.expect("Failed to unwrap arguments");
-
-        if s.eq("-v") || s.eq("--version") {
-            println!(
-                "\n  Transgender.rs\n\n    Regex-powered trans, version {}\n ",
-                VERSION
-            );
-            exit(0);
-        } else if s.eq("--sh") {
-            let script = r###"function ts() {
+static SHELL_SCRIPT: &str = r###"function ts() {
   dest_file=$(mktemp -t "ts.XXXXXXXXXXX")
   transgender $@ --dest "${dest_file}"
   dest_dir=$(tail -n 1 "${dest_file}")
@@ -77,7 +52,34 @@ then
   zstyle ':completion:*:*:ts:*' file-patterns '*(-/):directories'
 fi
 "###;
-            print!("{}", script);
+
+fn main() {
+    let mut path = String::from(".");
+    let mut dest_file: Option<String> = None;
+    let mut config_path: String;
+    let mut config_path_op: Option<&str> = None;
+
+    let mut args = env::args();
+    let print_help_message = || {
+        print!("{}", HELP_MSG);
+    };
+
+    loop {
+        let _s = args.next();
+        if _s.is_none() {
+            break;
+        }
+
+        let s = _s.expect("Failed to unwrap arguments");
+
+        if s.eq("-v") || s.eq("--version") {
+            println!(
+                "\n  Transgender.rs\n\n    Regex-powered trans, version {}\n ",
+                VERSION
+            );
+            exit(0);
+        } else if s.eq("--sh") {
+            print!("{}", SHELL_SCRIPT);
             exit(0);
         } else if s.eq("--dest") {
             let _one_more = args.next();
