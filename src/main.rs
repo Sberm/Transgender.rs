Sberm/Transgender.rs

commit bf772d0c633630947d8fc38ee1814c49d596da63

main mktemp: Remove -t option
Should not be -t XXXX because you'll get ts.XXXX.C8fi
instead of ts.C8fi.

Signed-off-by: Howard Chu <howardchu95@gmail.com>

src/main.rs
@@ -32,7 +32,7 @@ static HELP_MSG: &str = r###"
 "###;
 
 static SHELL_SCRIPT: &str = r###"function ts() {
-  dest_file=$(mktemp -t "ts.XXXXXXXXXXX")
+  dest_file=$(mktemp "ts.XXXXXXXXXXX")
   transgender $@ --dest "${dest_file}"
   dest_dir=$(tail -n 1 "${dest_file}")
   cd "${dest_dir}"

commit 855936637c9db5894eba232217fa2cb8851bd2f5

bug: Generate trans file in temp dir
-t uses $TMPDIR, I removed it, so it generates at CWD. Don't want that,
let's keep -t.

Fixes: bf772d0 "main mktemp: Remove -t option"

Signed-off-by: Howard Chu <howardchu95@gmail.com>

src/main.rs
@@ -32,7 +32,7 @@ static HELP_MSG: &str = r###"
 "###;
 
 static SHELL_SCRIPT: &str = r###"function ts() {
-  dest_file=$(mktemp "ts.XXXXXXXXXXX")
+  dest_file=$(mktemp -t ts)
   transgender $@ --dest "${dest_file}"
   dest_dir=$(tail -n 1 "${dest_file}")
   cd "${dest_dir}"